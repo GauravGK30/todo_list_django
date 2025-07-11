2395f83026f2636cfb085727fedc5ba981c58c34
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-11 18:17:22 +0530

    Update settings.py

--- to_do_project/todoapp/settings.py
+++ to_do_project/todoapp/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%-uun@4pb(&5w5j4qh((22nbi_fes22wr!ihktg9hquw&t*dy7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'todo_db',  
         'USER': 'root',      
-        'PASSWORD': 'root',  
+        'PASSWORD': '',  
         'HOST': 'localhost',
         'PORT': '3306',
     }
